c472401961b6d4a2dc136fd1657e92594a3de966
Lab User <LabUser@labs.salina.k-state.edu>
2018-11-13 10:59:48 -0600

    Added mLab database connection.

--- app.js
+++ app.js
@@ -10,6 +10,14 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+//Set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://Wes:locallibrary1986@ds161183.mlab.com:61183/twist-database';
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
