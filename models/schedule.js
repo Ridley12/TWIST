b0613527e7c99e76fc0046c635f9b5ed0266160e
Lab User <LabUser@labs.salina.k-state.edu>
2018-11-13 11:24:22 -0600

    Added schedule schema.

---
+++ models/schedule.js
@@ -0,0 +1,43 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var ScheduleSchema = new Schema(
+  {
+    session_number: {type: String, required: true, max: 100},
+    room_number: {type: String, required: true, max: 100},
+    topic_code: {type: String, required: true, max: 100},
+    presenter_id: {type: String, required: true, max: 100},
+  }
+);
+
+// Virtual for schedule's session number
+ScheduleSchema
+.virtual('session_number')
+.get(function () {
+  return this.session_number;
+});
+
+// Virtual for schedule's room_number
+ScheduleSchema
+.virtual('room_number')
+.get(function () {
+  return this.room_number;
+});
+
+// Virtual for schedule's topic code
+ScheduleSchema
+.virtual('topic_code')
+.get(function () {
+    return this.topic_code;
+});
+
+// Virtual for schedule's presenter id
+ScheduleSchema
+.virtual('presenter_id')
+.get(function () {
+    return this.presenter_id;
+});
+
+//Export model
+module.exports = mongoose.model('Schedule', ScheduleSchema);
